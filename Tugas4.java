1cd88161af761268326a15c5f1b4f1a4b20cebf8
AkmalNuurFauzan <120092353+AkmalNuurFauzan@users.noreply.github.com>
2023-03-19 06:15:10 +0700

    Fix tugas4
    Di bagian output lantai yang diperbaiki

--- Tugas4.java
+++ Tugas4.java
@@ -22,15 +22,13 @@ public class Tugas4 {
         int count = 0;
         for(int lantai = 0;lantai < hotel.length;lantai++){
             for(int kamar = 0; kamar < hotel[lantai].length;kamar++){
-//                for(int lantaiAkhir = hotel[hotel.length-1][0];lantaiAkhir>0;lantaiAkhir--){
                     if(hotel[lantai][kamar] == 'X'){
-                        System.out.println("Tamu berada di Lantai "+ (lantai+1) /* atau (lantai-4) */ +" Kamar "+ (kamar+1));
+                        System.out.println("Tamu berada di Lantai "+ (hotel.length-lantai) +" Kamar "+ (kamar+1));
                     }
                     if(hotel[lantai][kamar] == kosong){
                         count++;
                     }
                 }
-//            }
         }
         System.out.println("Jumlah kamar yang tersedia adalah "+count+" kamar");
     }    
